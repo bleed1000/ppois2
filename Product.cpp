3d093686af81c982d7014f54f9d5af34aaaf6802
bleed1000 <144164685+bleed1000@users.noreply.github.com>
2023-12-08 09:53:25 +0300

    Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,18 +1,18 @@
 namespace Minimarket {
 	class Product {
 	public:
-		Product(const std::string& productName, double productPrice, int productQuantity)
-			: name(productName), price(productPrice), quantity(productQuantity) {}
+		Product(const std::string& ProductName, double ProductPrice, int ProductQuantity)
+			: Name(ProductName), Price(ProductPrice), Quantity(ProductQuantity) {}
 
 		void GetProductDetaitls();
 
-		const std::string& GetName() const { return name; }
-		double GetPrice() const { return price; }
-		int GetQuantity() const { return quantity; }
+		const std::string& GetName() const { return Name; }
+		double GetPrice() const { return Price; }
+		int GetQuantity() const { return Quantity; }
 	private:
-		std::string name;
-		double price;
-		int quantity;
+		std::string Name;
+		double Price;
+		int Quantity;
 
 
 	};
