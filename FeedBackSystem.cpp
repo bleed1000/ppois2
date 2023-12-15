e3bb0e4a20036442947f7de41d77afde81c21222
bleed1000 <144164685+bleed1000@users.noreply.github.com>
2023-12-16 02:29:33 +0300

    Update FeedBackSystem.cpp

--- FeedBackSystem.cpp
+++ FeedBackSystem.cpp
@@ -1,11 +1,11 @@
 namespace Minimarket{
 	class FeedBackSystem : protected Customer {
 	protected:
-		std::string Feedback;
-		int Rate;
+		std::string feedback;
+		int rate;
 	public:
-		FeedBackSystem(const std::string& CustomerId, const std::string& Name, const std::string& Email, std::string Feedback, int Rate)
-			: Customer(CustomerId, Name, Email), Feedback(Feedback), Rate(Rate) {}
+		FeedBackSystem(const std::string& customerId, const std::string& name, const std::string& email, std::string feedback, int rate)
+			: Customer(customerId, name, email), feedback(feedback), rate(rate) {}
 		void ReceivedFeedback();
 	};
 }
