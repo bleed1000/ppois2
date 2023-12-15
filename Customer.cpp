a956d54a6592eb1d801ba1ba3d5b3b99cbb7e709
bleed1000 <144164685+bleed1000@users.noreply.github.com>
2023-12-16 02:28:24 +0300

    Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -2,14 +2,14 @@ namespace Minimarket{
 	class Customer
 	{
 	protected:
-		std::string CustomerId;
-		std::string Name;
-		std::string Email;
+		std::string customerId;
+		std::string name;
+		std::string email;
 	public:
-		Customer(const std::string& CustomerCustomerId, const std::string& CustomerName, const std::string& CustomerEmail)
-			: CustomerId(CustomerCustomerId), Name(CustomerName), Email(CustomerEmail) {}
+		Customer(const std::string& customerCustomerId, const std::string& customerName, const std::string& customerEmail)
+			: customerId(CustomerCustomerId), name(CustomerName), email(customerEmail) {}
 		void place_order(const Product& product);
 
-		std::string GetName() const { return Name; }
+		std::string GetName() const { return name; }
 	};
 }
